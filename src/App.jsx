b65aa33f9cbd87114d727fb6c191a6bb7f1eaeec
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-06-11 13:05:07 +0530

    Add main layout and sections to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 
 
 function App() {
-  
+    return(
+        <div className="min-h-screen overflow-x-hidden">
+            <Navbar/>
+            <main>
+                
+            </main>
+        </div>
+    )
 }
 
 export default App
